d3b159726fca6ffc4fc6704ad9d23a7ced0b3ecb
Alice <alicemariaferreiradelima@gmail.con>
2022-02-15 15:21:06 -0300

    Primero commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const apiData = {
+    url: 'https://api.dictionaryapi.dev/api/v2/entries/en',
+    word: 'hello',
+}
+
+const {url, word} = apiData;
+const apiUrl = `${url}/${word}`
+
+console.log(apiUrl)
+
+fetch(apiUrl)
+    .then((data) => data.json())
