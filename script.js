6d164ea279bc37d5042c46b87038030a551c6e17
Alice Maria <alice.lima@imd.ufrn.br>
2022-02-16 17:16:42 -0300

    Utilizando a api

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-const apiData = {
-    url: 'https://api.dictionaryapi.dev/api/v2/entries/en',
-    word: 'hello',
+$(document).ready(function(){
+    $("#form-submit").submit(function(event){
+        performSearch(event);
+    });
+});
+
+function performSearch(event) {
+    var request;
+    event.preventDefault();
+    $("#the-word").text("welcome");
+
+    request = $.ajax({
+        url: 'https://api.dictionaryapi.dev/api/v2/entries/en/',
+        type: "GET",
+        data: $("#word").val(),
+    });
+
+    request.done(function(response){
+        console.log(response);
+        formatSearch(response);
+        console.log(response);
+    });
+
+    request.fail(function (){
+        $("#the-word").text("Please try again, incorrect input!");
+    });
 }
 
-const {url, word} = apiData;
-const apiUrl = `${url}/${word}`
-
-console.log(apiUrl)
-
-fetch(apiUrl)
-    .then((data) => data.json())
-    .then((hello) => console.log(hello))
+function formatSearch(jsonObject) {
+    var the_word = jsonObject.word;
+    var the_phonetic = jsonObject.phonetic;
+    console.log(word);
+    
+    $("#the-word").text(the_word);
+    $("#the-phonetic").text(the_phonetic);
+}
